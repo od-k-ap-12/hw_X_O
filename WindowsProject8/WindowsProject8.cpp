f291a769b7bd611a40ec783097369c3220f8bd68
Oleksandra <predictormagnolia@gmail.com>
2022-11-11 23:44:18 +0200

    Add project files.

---
+++ WindowsProject8/WindowsProject8.cpp
@@ -0,0 +1,198 @@
+﻿// WindowsProject8.cpp : Defines the entry point for the application.
+//
+
+#include "framework.h"
+#include "WindowsProject8.h"
+
+#define MAX_LOADSTRING 100
+
+BOOL CALLBACK DlgProc(HWND, UINT, WPARAM, LPARAM);
+
+// Global Variables:
+HINSTANCE hInst;                                // current instance
+WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
+WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+
+HWND hClick[9], hPic[9];
+HBITMAP hBmp[2];
+
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    return DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc);
+}
+
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    //wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WINDOWSPROJECT8));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_WINDOWSPROJECT8);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // Store instance handle in our global variable
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+
+BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wp, LPARAM lp) {
+
+    TCHAR str[100];
+    int Answers[9];
+    static int Order = 0;
+    switch (message)
+    {
+    case WM_CLOSE:
+        EndDialog(hWnd, 0);
+        return TRUE;
+
+    case  WM_INITDIALOG:
+        for (int i = 0; i < 9; i++) {
+            hClick[i] = GetDlgItem(hWnd, IDC_BUTTON1+i);
+        }
+        for (int i = 0; i < 9; i++) {
+            hPic[i] = GetDlgItem(hWnd, IDC_STATIC1 + i);
+        }
+        hBmp[0] = LoadBitmap(GetModuleHandle(NULL), MAKEINTRESOURCE(IDB_BITMAP1)); //Х
+        hBmp[1] = LoadBitmap(GetModuleHandle(NULL), MAKEINTRESOURCE(IDB_BITMAP2)); //О
+        return TRUE;
+
+    case WM_COMMAND: {
+        if (LOWORD(wp) == IDC_BUTTON1)
+        {
+            if (Order == 0) {
+                SendMessage(hPic[0], STM_SETIMAGE, WPARAM(IMAGE_BITMAP), LPARAM(hBmp[0]));
+                Order = 1;
+            }
+            else {
+                SendMessage(hPic[0], STM_SETIMAGE, WPARAM(IMAGE_BITMAP), LPARAM(hBmp[1]));
+                Order = 0;
+            }
+        }
+        if (LOWORD(wp) == IDC_BUTTON2)
+        {
+            if (Order == 0) {
+                SendMessage(hPic[1], STM_SETIMAGE, WPARAM(IMAGE_BITMAP), LPARAM(hBmp[0]));
+                Order = 1;
+            }
+            else {
+                SendMessage(hPic[1], STM_SETIMAGE, WPARAM(IMAGE_BITMAP), LPARAM(hBmp[1]));
+                Order = 0;
+            }
+        }
+        if (LOWORD(wp) == IDC_BUTTON3)
+        {
+            if (Order == 0) {
+                SendMessage(hPic[2], STM_SETIMAGE, WPARAM(IMAGE_BITMAP), LPARAM(hBmp[0]));
+                Order = 1;
+            }
+            else {
+                SendMessage(hPic[2], STM_SETIMAGE, WPARAM(IMAGE_BITMAP), LPARAM(hBmp[1]));
+                Order = 0;
+            }
+        }
+        if (LOWORD(wp) == IDC_BUTTON4)
+        {
+            if (Order == 0) {
+                SendMessage(hPic[3], STM_SETIMAGE, WPARAM(IMAGE_BITMAP), LPARAM(hBmp[0]));
+                Order = 1;
+            }
+            else {
+                SendMessage(hPic[3], STM_SETIMAGE, WPARAM(IMAGE_BITMAP), LPARAM(hBmp[1]));
+                Order = 0;
+            }
+        }
+        if (LOWORD(wp) == IDC_BUTTON5)
+        {
+            if (Order == 0) {
+                SendMessage(hPic[4], STM_SETIMAGE, WPARAM(IMAGE_BITMAP), LPARAM(hBmp[0]));
+                Order = 1;
+            }
+            else {
+                SendMessage(hPic[4], STM_SETIMAGE, WPARAM(IMAGE_BITMAP), LPARAM(hBmp[1]));
+                Order = 0;
+            }
+        }
+        if (LOWORD(wp) == IDC_BUTTON6)
+        {
+            if (Order == 0) {
+                SendMessage(hPic[5], STM_SETIMAGE, WPARAM(IMAGE_BITMAP), LPARAM(hBmp[0]));
+                Order = 1;
+            }
+            else {
+                SendMessage(hPic[5], STM_SETIMAGE, WPARAM(IMAGE_BITMAP), LPARAM(hBmp[1]));
+                Order = 0;
+            }
+        }
+        if (LOWORD(wp) == IDC_BUTTON7)
+        {
+            if (Order == 0) {
+                SendMessage(hPic[6], STM_SETIMAGE, WPARAM(IMAGE_BITMAP), LPARAM(hBmp[0]));
+                Order = 1;
+            }
+            else {
+                SendMessage(hPic[6], STM_SETIMAGE, WPARAM(IMAGE_BITMAP), LPARAM(hBmp[1]));
+                Order = 0;
+            }
+        }
+        if (LOWORD(wp) == IDC_BUTTON8)
+        {
+            if (Order == 0) {
+                SendMessage(hPic[7], STM_SETIMAGE, WPARAM(IMAGE_BITMAP), LPARAM(hBmp[0]));
+                Order = 1;
+            }
+            else {
+                SendMessage(hPic[7], STM_SETIMAGE, WPARAM(IMAGE_BITMAP), LPARAM(hBmp[1]));
+                Order = 0;
+            }
+        }
+        if (LOWORD(wp) == IDC_BUTTON9)
+        {
+            if (Order == 0) {
+                SendMessage(hPic[8], STM_SETIMAGE, WPARAM(IMAGE_BITMAP), LPARAM(hBmp[0]));
+                Order = 1;
+            }
+            else {
+                SendMessage(hPic[8], STM_SETIMAGE, WPARAM(IMAGE_BITMAP), LPARAM(hBmp[1]));
+                Order = 0;
+            }
+        }
+    }
+    return TRUE;
+    }
+    return FALSE;
+}
---
+++ WindowsProject8/resource.h
@@ -0,0 +1,48 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by WindowsProject8.rc
+//
+#define IDC_MYICON                      2
+#define IDD_WINDOWSPROJECT8_DIALOG      102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_WINDOWSPROJECT8             107
+#define IDI_SMALL                       108
+#define IDC_WINDOWSPROJECT8             109
+#define IDR_MAINFRAME                   128
+#define IDD_DIALOG1                     129
+#define IDB_BITMAP1                     130
+#define IDB_BITMAP2                     131
+#define IDB_BITMAP3                     132
+#define IDC_BUTTON1                     10
+#define IDC_BUTTON2                     11
+#define IDC_BUTTON3                     12
+#define IDC_BUTTON4                     13
+#define IDC_BUTTON5                     14
+#define IDC_BUTTON6                     15
+#define IDC_BUTTON7                     16
+#define IDC_BUTTON8                     17
+#define IDC_BUTTON9                     18
+#define IDC_STATIC1                     1013
+#define IDC_STATIC2                     1014
+#define IDC_STATIC3                     1015
+#define IDC_STATIC4                     1016
+#define IDC_STATIC5                     1017
+#define IDC_STATIC6                     1018
+#define IDC_STATIC7                     1019
+#define IDC_STATIC8                     1020
+#define IDC_STATIC9                     1021
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        133
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
